7d0b21d59361a647a7ccaebadf9ada403a4c74b2
Bobby Iliev <bobby@bobbyiliev.com>
2024-09-29 11:22:05 +0300

    Add deep links to release notes

--- src/components/atoms/Bug.tsx
+++ src/components/atoms/Bug.tsx
@@ -1,5 +1,6 @@
 import type data from "@/data/pg_release_data";
 import { getPgVersionDate } from "@/utils/postgresDates";
+import { generatePgDeepLink } from "@/utils/pgDeepLinks";
 
 type PromiseResulver<T> = T extends Promise<infer U> ? U : never;
 type Bug = PromiseResulver<typeof data>["bugs"][0];
@@ -16,19 +17,25 @@ export default function Bug({ bug }: { bug: Bug }) {
 
     const date = getPgVersionDate(bug.fixedIn);
     const fixed = `Fixed on ${date.toLocaleDateString()}`;
+    const deepLink = generatePgDeepLink(bug.fixedIn, bug.title);
 
     return (
-        <div className="border-2 border-code-red-1 p-2 text-center max-w-64">
+        <div className="p-2 text-center border-2 border-code-red-1 max-w-64">
             {
                 bug.cve && (
-                    <h3 className="font-title text-xl select-all mb-2">
+                    <h3 className="mb-2 text-xl select-all font-title">
                         <a href={`https://www.cve.org/CVERecord?id=${encodeURIComponent(bug.cve)}`} target="_blank" rel="noopener noreferrer">
                             {bug.cve}
                         </a>
                     </h3>
                 )
             }
-            <h4 className="font-title text-lg select-all mb-2">{bug.title} ({fixed})</h4>
+            <h4 className="mb-2 text-lg select-all font-title">
+                <a href={deepLink} target="_blank" rel="noopener noreferrer" className="hover:underline">
+                    {bug.title}
+                </a>
+                {" "}({fixed})
+            </h4>
             <p className="select-none">{bug.description}{contributors}</p>
         </div>
     );
