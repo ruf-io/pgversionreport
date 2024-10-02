bbe8c6f322c62664f67efb064fa6640827d8af83
Astrid Gealer <astrid@gealer.email>
2024-10-03 00:35:44 +0100

    fix the rendered server content

--- serverRenderer.tsx
+++ serverRenderer.tsx
@@ -2,11 +2,30 @@ import ReactDOMServer from "react-dom/server";
 import App from "./src/components/App";
 import { JSDOM } from "jsdom";
 import { readFileSync, writeFileSync } from "fs";
+import { Writable } from "stream";
 
-const content = ReactDOMServer.renderToString(<App />);
+const piper = ReactDOMServer.renderToPipeableStream(<App />);
+class StringWritable extends Writable {
+    content = "";
+    endCb?: () => void;
 
-const dom = new JSDOM(readFileSync("dist/index.html", "utf-8"));
-dom.window.document.getElementById("app")!.innerHTML = content;
-writeFileSync("dist/index.html", dom.serialize());
+    _write(chunk: any, _: string, callback: (error?: Error | null) => void) {
+        this.content += chunk.toString();
+        callback();
+    }
 
-console.log("Server rendered the App content!");
+    _final(callback: (error?: Error | null) => void) {
+        this.endCb?.();
+        callback();
+    }
+}
+const writer = new StringWritable();
+
+writer.endCb = () => {
+    const dom = new JSDOM(readFileSync("dist/index.html", "utf-8"));
+    dom.window.document.getElementById("app")!.innerHTML = writer.content;
+    writeFileSync("dist/index.html", dom.serialize());
+    console.log("Server rendered the App content!");
+};
+
+piper.pipe(writer);
