b9ff77099d0731988a70c1d59753feb1443d5a75
Astrid Gealer <astrid@gealer.email>
2024-10-03 19:31:27 +0100

    add linting on push

--- src/components/molecules/HowToUpgrade.tsx
+++ src/components/molecules/HowToUpgrade.tsx
@@ -3,13 +3,12 @@ export default function HowToUpgrade() {
         <div className="prose">
             <h3 className="text-xl font-bold">Upgrade on Managed Hosts</h3>
             <p>
-                Please see the documentation for your managed PostgreSQL provider:
+                Please see the documentation for your managed PostgreSQL
+                provider:
             </p>
             <ul>
                 <li>
-                    <a href="https://neon.tech/blog/postgres-17">
-                        Neon
-                    </a>
+                    <a href="https://neon.tech/blog/postgres-17">Neon</a>
                 </li>
                 <li>
                     <a href="https://supabase.com/docs/guides/platform/migrating-and-upgrading-projects">
@@ -27,18 +26,23 @@ export default function HowToUpgrade() {
                     </a>
                 </li>
                 <li>
-                    <a href="https://xata.io/docs/postgres">
-                        Xata
-                    </a>
-                    {" "}(The Postgres version depends on your deployment and plan. In a shared cluster, you operate in a multi tenant environment, sharing the same Postgres version. Minor version upgrades are continuously updated. If you want control of the version, you should consider a dedicated cluster.)
+                    <a href="https://xata.io/docs/postgres">Xata</a> (The
+                    Postgres version depends on your deployment and plan. In a
+                    shared cluster, you operate in a multi tenant environment,
+                    sharing the same Postgres version. Minor version upgrades
+                    are continuously updated. If you want control of the
+                    version, you should consider a dedicated cluster.)
                 </li>
             </ul>
 
             <h3 className="text-xl font-bold">Upgrade on Self-Managed Hosts</h3>
             <p>
-                To upgrade PostgreSQL on a self-managed host, you can <a href="https://www.postgresql.org/docs/current/upgrading.html">
+                To upgrade PostgreSQL on a self-managed host, you can{" "}
+                <a href="https://www.postgresql.org/docs/current/upgrading.html">
                     follow the official PostgreSQL documentation
-                </a>. The distribution of your version will depend on your operating systems package manager.
+                </a>
+                . The distribution of your version will depend on your operating
+                systems package manager.
             </p>
         </div>
     );
